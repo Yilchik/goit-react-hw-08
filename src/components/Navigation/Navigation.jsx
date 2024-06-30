22412a3fcb673a079a9bce0140ff66bcbdb1f929
Yilchik <yulyayulia2@gmail.com>
2024-06-30 15:54:34 +0200

    Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  const isLoggedIn = useSelector(selectLoggedIn);
+  return (
+    <nav>
+      <ul className="flex">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {isLoggedIn && (
+          <li>
+            <Link to="/tasks">Tasks</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
