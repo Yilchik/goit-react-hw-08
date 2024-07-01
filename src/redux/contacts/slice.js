a7f147f77f934cfecc313236856af9c837801f8d
Yilchik <yulyayulia2@gmail.com>
2024-07-01 19:56:43 +0200

    instal

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,4 +1,4 @@
-import { createSelector, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { addContact, deleteContact, fetchContacts } from "./operations";
 
 const contactsSlice = createSlice({
